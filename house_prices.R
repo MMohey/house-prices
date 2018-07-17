40f7b3d11374964dd448e3acc48557bdf4c1fdf2
mmohey <moh.mafifi@gmail.com>
2018-07-17 19:17:54 +0200

    added correlation part

--- house_prices.R
+++ house_prices.R
@@ -9,32 +9,76 @@ library(readr)
 library(data.table)
 library(GGally)
 library(caret)
+library(gridExtra)
+library(scales)
+library(corrplot)
+library(ggrepel)
 
 
-raw_train <- read_csv("train.csv")
-raw_test <- read_csv("test.csv")
-full <- bind_rows(raw_train, raw_test)
+train_raw <- read_csv("train.csv")
+test_raw <- read_csv("test.csv")
 
+
+str(train_raw)
+str(test_raw)
+
+test_raw$SalePrice <- NA
+full <- bind_rows(train_raw, test_raw)
 length(unique(full$Id)) == length(full$Id) ## check no duped entries
 
-str(full)
+test_Id <- test_raw$Id
+
+full$Id <- NULL
+
 summary(full)
 # To check for missing values
 sapply(full, function(x) sum(is.na(x) | x ==""))
 
-# Unique values per column
-sapply(full, function(x) length(unique(x))) 
+# Histograg plot for the response var
+ggplot(full[!is.na(full$SalePrice),],aes(x=SalePrice) ) 
++ geom_histogram(bins = 50) 
++ scale_x_continuous(breaks = seq( 0, max(full$SalePrice,na.rm = TRUE), 
+                                     by = 100000), labels = comma)
+
+# Separating Numeric and Char vars
+num_vars <- which(sapply(full, is.numeric))
+char_vars <- which(sapply(full, is.character))
+
+# Numeric vars correlation with SalesPrice
+# sort the cor matrix and then limit to cor > 0.5
+num_vars_cor <- cor(full[,num_vars],use = "pairwise.complete.obs")
+cor_sorted <- as.matrix(sort(num_vars_cor[,'SalePrice'], decreasing = TRUE))
+high_cor <- cor_sorted[abs(cor_sorted) > 0.5, ]
+high_cor_mat <- num_vars_cor[names(high_cor), names(high_cor)]
+corrplot.mixed(high_cor_mat, tl.col="black", tl.pos = "lt")
 
 
-missing_values <- full %>% summarize_all(funs(sum(is.na(.))/n()))
+# Plot cor between highest two vars vs SalePrice
+ggplot(full[!is.na(full$SalePrice), ],aes(x=factor(OverfullQual), y= SalePrice))
++ geom_boxplot(col = 'blue') 
++ scale_y_continuous(breaks = seq(0,800000, by = 100000), labels = comma)
 
-missing_values <- gather(missing_values, key="feature", value="missing_pct")
-missing_values %>% 
-    ggplot(aes(x=reorder(feature,-missing_pct),y=missing_pct)) +
-    geom_bar(stat="identity",fill="red")+
-    coord_flip()+theme_bw()
 
-setdiff(names(train), names(test)) 
+ggplot(data=full[!is.na(full$SalePrice),], aes(x=GrLivArea, y=SalePrice))
++ geom_point(col='blue') 
++ geom_smooth(method = "lm", se=FALSE, color="black", aes(group=1)) 
++ scale_y_continuous(breaks= seq(0, 800000, by=100000), labels = comma) 
++ geom_text_repel(aes(label = ifelse
+                      (full$GrLivArea[!is.na(full$SalePrice)]>4500,
+                          rownames(full), '')))
+# # Unique values per column
+# sapply(full, function(x) length(unique(x))) 
+# 
+# 
+# missing_values <- full %>% summarize_all(funs(sum(is.na(.))/n()))
+# 
+# missing_values <- gather(missing_values, key="feature", value="missing_pct")
+# missing_values %>% 
+#     ggplot(aes(x=reorder(feature,-missing_pct),y=missing_pct)) +
+#     geom_bar(stat="identity",fill="red")+
+#     coord_flip()+theme_bw()
+# 
+# setdiff(names(train_raw), names(test_raw)) 
 
 #full %>% map_dbl(~sum(is.na(.)))
 
